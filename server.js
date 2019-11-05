2278cff23abcfaa5c8186ec14d10e598b427876a
santhoshkumarhere <santhoshkumarhere@gmail.com>
2019-11-05 17:41:20 -0600

    test

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
     // Initialize the app.
       const server = app.listen(process.env.PORT || 8080, () => {
       const port = server.address().port;
-      console.log("App now running on port", port);
+      console.log("App now running on set port", port);
     });
 });
 
