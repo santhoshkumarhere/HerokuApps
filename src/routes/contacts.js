4002215c61f59ec0d44e054b36415870fe66ca30
Kumar, Santhosh <santhoshk.kumar@pioneer.com>
2019-11-06 10:00:13 -0600

    error handler

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -7,16 +7,64 @@ router.get("/api/contacts", async function(req, res) {
     const db = req.app.db;
     db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
       if (err) {
-        handleError(res, err.message, "Failed to get contacts.");
+        next(err);
       } else {
         res.status(200).json(docs);
       }
     });
   });
 
-  function handleError(res, reason, message, code) {
-    console.log("ERROR from contacts.js: " + reason);
-    res.status(code || 500).json({"error": message});
-}
 
+  router.post("/api/contacts", async function(req, res) {
+    var newContact = req.body;
+    newContact.createDate = new Date();
+  
+    if (!req.body.name) {
+        next(err);
+    } else {
+      db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+        if (err) {
+            next(err);
+        } else {
+          res.status(201).json(doc.ops[0]);
+        }
+      });
+    }
+  });
+  
+  
+  router.get("/api/contacts/:id", async function(req, res) {
+      db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+        if (err) {
+            next(err);
+        } else {
+          res.status(200).json(doc);
+        }
+      });
+    });
+    
+    router.put("/api/contacts/:id", async function(req, res) {
+      var updateDoc = req.body;
+      delete updateDoc._id;
+    
+      db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, {$set: updateDoc}, function(err, doc) {
+        if (err) {
+            next(err);
+        } else {
+          updateDoc._id = req.params.id;
+          res.status(200).json(updateDoc);
+        }
+      });
+    });
+    
+    router.delete("/api/contacts/:id", async function(req, res) {
+      db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+        if (err) {
+         next(err);
+        } else {
+          res.status(200).json(req.params.id);
+        }
+      });
+    });
+  
 module.exports = router;
